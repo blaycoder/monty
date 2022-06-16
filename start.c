4e6618d611498911cbb3815c7c342a875d0133c6
blaycoder <onatola4real@gmail.com>
2022-06-16 15:53:05 -0700

    Added a tasks

---
+++ start.c
@@ -0,0 +1,27 @@
+#define  _POSIX_C_SOURCE 200809L
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "monty.h"
+
+void file_error(char *argv);
+void error_usage(void);
+int status = 0;		/* global var declaration */
+
+/**
+ * main - entry point
+ * @argv: list of arguments passed to our program
+ * @argc: amount of args
+ *
+ * Return: nothing
+ */
+int main(int argc, char **argv)
+{
+	FILE *file;
+	size_t buf_len = 0;
+	char *buffer = NULL;
+	char *str = NULL;
+	stack_t *stack = NULL;
+	unsigned int line_cnt = 1;
+
+	global.data_struct = 1;  /* struct defined in monty.h L58*/
